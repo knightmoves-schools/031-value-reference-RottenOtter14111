6a5f200ce016570d89bdb189272df9eb7936556e
RottenOtter14111 <arogers@anamosa.k12.ia.us>
2026-03-31 03:32:04 +0000

    WOP

--- Account.cs
+++ Account.cs
@@ -5,5 +5,18 @@ public class Account {
     public static int[] CurrentLedger = {10, 20, 30, 40, 50};
 
     // Add your code here
+    public static int NewTaxRate = OriginalTaxRate;
+    public static int[] CopyLedger = CurrentLedger;
+
+    public int[] UpdateLedger(int correction)
+    {
+        CurrentLedger[0] = correction;
+        return CurrentLedger;
+    }
+    public int AdjustTaxRate(int taxRate)
+    {
+        NewTaxRate = taxRate;
+        return NewTaxRate;
+    }
 }
 
