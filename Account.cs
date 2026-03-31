4a459b5ef5a9925dd6633dcc84f0669ae5aa4521
RottenOtter14111 <arogers@anamosa.k12.ia.us>
2026-03-31 03:38:23 +0000

    WOAAAA

--- Account.cs
+++ Account.cs
@@ -1,22 +1,23 @@
 ﻿namespace knightmoves;
 
-public class Account {
+public class Account 
+{
     public static int OriginalTaxRate = 10;
-    public static int[] CurrentLedger = {10, 20, 30, 40, 50};
+    public static int[] CurrentLedger = { 10, 20, 30, 40, 50 };
 
-    // Add your code here
+    
     public static int NewTaxRate = OriginalTaxRate;
     public static int[] CopyLedger = CurrentLedger;
 
     public int[] UpdateLedger(int correction)
     {
-        CurrentLedger[0] = correction;
-        return CurrentLedger;
+        CurrentLedger[0] = correction;     
+        return CurrentLedger;              
     }
+
     public int AdjustTaxRate(int taxRate)
     {
-        NewTaxRate = taxRate;
-        return NewTaxRate;
+        NewTaxRate = taxRate;             
+        return NewTaxRate;                
     }
 }
-
